846f95bcef0cca19910037755b62b4f001090950
KADOI021 <menjingxile@gmail.com>
2025-08-19 20:00:43 +0900

    second commit by TortoseGit

--- gitHubTest.java
+++ gitHubTest.java
@@ -4,5 +4,6 @@ public class gitHubTest {
 	public static void main(String[] args) {
 		System.out.println("Test GitHub");
 		System.out.println("this text was added on GitHub");
+		System.out.println("今度は、TortoseGitでpushしてみるよ。");
 	}
 }
