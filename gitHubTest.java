10c044dc3540d3b2e01c178d4ed0e991d812e7d2
KADOI021 <menjingxile@gmail.com>
2025-08-19 19:26:55 +0900

    GitHub上でコードの修正を行ったので、pullSITENE
    I add a console text code on GitHub

--- gitHubTest.java
+++ gitHubTest.java
@@ -3,5 +3,6 @@ package Test;
 public class gitHubTest {
 	public static void main(String[] args) {
 		System.out.println("Test GitHub");
+		System.out.println("this text was added on GitHub");
 	}
 }
